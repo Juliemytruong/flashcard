053e1bf4b404bea6e14e00379b3d804cf726bcda
julie <juliemytruong@gmail.com>
2024-08-03 14:16:56 +1000

    sdf

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ BACKGROUND_COLOR = "#B1DDC6"
 from tkinter import*
 import random
 import pandas
+text_f=0
 
 #-------------------setup data----------------#
 with open("french_words.csv","r") as data:
@@ -13,26 +14,31 @@ print (dataframe)
 
 #------------------Random word------------------#
 
-num=random.randint(0,len(dataframe))
-card=dataframe.iloc[num]
-f_card=dataframe.loc[num,"French"]
-e_card=dataframe.loc[num,"English"]
+def word():
+
+    canvas.delete("text")
+    num=random.randint(0,len(dataframe))
 
 
-print(card)
-print(f_card)
-print(e_card)
+    data_dic=dataframe.to_dict(orient="records")
+    print(data_dic)
+    data_c=data_dic[num]
+    french_c=data_c.get("French")
+    print(french_c)
+
+    canvas.create_text(400,250,text="french",font=("Ariel", 40, "italic"))
+    canvas.create_text(400,363,text=french_c,font=("Ariel", 65, "bold"),tags="text")
 
 
-data_dic=dataframe.to_dict(orient="records")
-print(data_dic)
 
 
-#canvas.create_text(400,250,text="hello",font=("Ariel", 40, "italic"))
-#canvas.create_text(400,363,text="hello again",font=("Ariel", 65, "bold"))
+
 
 #--------------------Right--------------------------#
-
+# def right():
+#     global text_f
+#     canvas.delete("text_f")
+#     word()
 
 
 
@@ -48,31 +54,24 @@ canvas=Canvas(width=800,height=580)
 card_side=PhotoImage(file="card_front.png")
 canvas.create_image(400,320,image=card_side)
 canvas.config(bg=BACKGROUND_COLOR, highlightbackground=BACKGROUND_COLOR)
-canvas.create_text(400,250,text="hello",font=("Ariel", 40, "italic"))
-canvas.create_text(400,363,text="hello again",font=("Ariel", 65, "bold"))
+#canvas.create_text(400,250,text="hello",font=("Ariel", 40, "italic"))
+#canvas.create_text(400,363,text="hello again",font=("Ariel", 65, "bold"))
+#text_f = canvas.create_text(400, 363, text="start", font=("Ariel", 65, "bold"))
 
 
 canvas.grid(column=1,row=1,columnspan=2,padx=10,pady=10)
 
 right_image = PhotoImage(file="right.png")
 buttonR = Button(image=right_image, highlightthickness=4,bg=BACKGROUND_COLOR)
-
 buttonR.grid(column=1,row=2,padx=50,pady=50)
 
 wrong_image = PhotoImage(file="wrong.png")
-buttonW = Button(image=wrong_image, highlightthickness=4,bg=BACKGROUND_COLOR)
+buttonW = Button(image=wrong_image, highlightthickness=4,bg=BACKGROUND_COLOR,command=word)
 buttonW.grid(column=2,row=2,padx=50,pady=50)
 
 
 
 #my_image = PhotoImage(file="path/to/image_file.png")
-
-
-
-
-
-
-
 
 
 
